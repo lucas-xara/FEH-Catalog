13d5b0320d78718945b70333bdde7faccf322b62
lucas-xara <lucasxara@gmail.com>
2025-08-25 18:38:59 -0300

    removing old project files (Misfired)

--- legacy/src/pages/HeroDetail.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function HeroDetail() {
-  const { id } = useParams();
-  return <div style={{ padding: 16 }}>Hero detail: {id}</div>;
-}
